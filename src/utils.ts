e1f8fd97a15b131d982ba02a4a41d9bf66ab18e1
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2024-12-03 19:33:59 -0500

    chore(utils): support reading example parts

--- src/utils.ts
+++ src/utils.ts
@@ -5,10 +5,24 @@ const EXAMPLES_DIR = path.join(import.meta.dirname, "../data/examples");
 
 export type SolutionFn = (input: string) => string;
 
-export const readExample = async (day: number) => {
-  const content = await readFile(
-    path.join(EXAMPLES_DIR, `${day.toString().padStart(2, "0")}.txt`),
-    "utf-8",
-  );
-  return content;
+/**
+ * Reads the corresponding text file in the examples directory for the provided day and part, and returns it as a string.
+ * @param day The day corresponding to the example
+ * @param part If provided, will attempt to read example files with `"-n"` appended after the day, where `n` is the part.
+ *
+ * For example, for `day=2` and `part=2`, the following file will be read and returned as a string:
+ *
+ * ```
+ * data/examples/02-2.txt
+ * ```
+ *
+ * @returns content of the example
+ */
+export const readExample = async (day: number, part?: number) => {
+  const paddedDay = day.toString().padStart(2, "0");
+  const pathToExample =
+    part !== undefined
+      ? path.join(EXAMPLES_DIR, `${paddedDay}-${part.toString()}.txt`)
+      : path.join(EXAMPLES_DIR, `${paddedDay}.txt`);
+  return readFile(pathToExample, "utf-8");
 };
