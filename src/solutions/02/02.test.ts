1656d8392af87137a38cae14f93c26627fb699b8
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2024-12-04 09:12:28 -0500

    test: add additional example test cases

--- src/solutions/02/02.test.ts
+++ src/solutions/02/02.test.ts
@@ -1,13 +1,25 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { readExample } from "../../utils";
 import { partOne, partTwo } from "./02";
 
 test("part 1", async () => {
-  const input = await readExample(2);
+  const input = await readExample(2, 1);
   expect(partOne(input)).toEqual("2");
 });
 
-test("part 2", async () => {
-  const input = await readExample(2);
-  expect(partTwo(input)).toEqual("4");
+describe("part 2", () => {
+  test("example 1 - official", async () => {
+    const input = await readExample(2, 1);
+    expect(partTwo(input)).toEqual("4");
+  });
+
+  test("example 2 - created, 2 numbers same at the start", async () => {
+    const input = await readExample(2, 2);
+    expect(partTwo(input)).toEqual("1");
+  });
+
+  test("example 3 - created, 2 numbers same at the end", async () => {
+    const input = await readExample(2, 3);
+    expect(partTwo(input)).toEqual("2");
+  });
 });
